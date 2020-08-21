0e9c2da5dedd5edf97d65f98e98de87662dc65e1
Sandeep Singh <sandeepsinghgour0@gmailo.com>
2020-08-21 20:25:13 +0530

    challenge complete

---
+++ index.js
@@ -0,0 +1,43 @@
+
+const Searchbox = document.getElementById("#search");
+const finalResult = document.getElementById("#result");
+let result = '';
+
+$("#form").submit(function (e){
+
+e.preventDefault()
+
+
+    
+    var query = $("#search").val()
+    const API_KEY = '992fd49bf034fc7e795ef4f189049b7a';
+
+    let url = `http://api.serpstack.com/search?access_key=${API_KEY}&type=web&query=${query}`;
+
+    fetch(url).then((res)=>
+    {
+        return res.json();
+    })
+    .then((data)=>{
+
+        $("#result").html('')
+
+        console.log(data);
+
+        data.organic_results.forEach(resp => {
+
+            result = `
+
+            <h1>${resp.title}</h1>
+            <br>
+            <a target="_blank" href="${resp.url}">${resp.url}</a>
+            <p>${resp.snippet}</p>
+    
+           `
+           $("#result").append(result)
+
+        });
+       
+
+    })
+})
